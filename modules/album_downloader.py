dfdc6869116b9c76ebaf784c509bba18577e30ad
mrartur343 <arturminesk@gmail.com>
2024-05-11 00:03:40 +0300

    spotify downloader2

--- modules/album_downloader.py
+++ modules/album_downloader.py
@@ -1,7 +1,10 @@
 import os
 
-def download_album(album_url:str,album_key:str):
+async def download_album(album_url:str,album_key:str):
 	os.system(f'spotdl {album_url}')
-	for file in os.listdir('modules'):
-		if file.endswith('.mp3'):
-			os.replace(f'modules/{file}', f'songs/{album_key}/{file}')
+	mp3_check = True
+	while mp3_check:
+		for file in os.listdir('modules'):
+			if file.endswith('.mp3'):
+				os.replace(f'modules/{file}', f'songs/{album_key}/{file}')
+				mp3_check=False
