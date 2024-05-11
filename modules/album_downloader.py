b0916d5da1074eff1ff7829a357204a4a0bb1687
mrartur343 <arturminesk@gmail.com>
2024-05-11 13:24:12 +0300

    albums_downloader_command.py bugfix

--- modules/album_downloader.py
+++ modules/album_downloader.py
@@ -1,23 +1,8 @@
-import asyncio
 import os
-import time
-
-import discord
 
 
 def download_album(album_url:str,album_key:str,log_channel=None):
-	os.system(f'spotdl --port 1488 {album_url}')
-	wait_check = True
-	timer = time.time()
-	while wait_check or timer-time.time()>-60:
-		asyncio.sleep(5)
-		filenames = os.listdir(".")
-		for file in filenames:
-			if file.endswith('.mp3'):
-				os.replace(file, f'songs/{album_key}/{file}')
-				timer = time.time()
-				wait_check=False
-				print(f"До альбому довантажено {file}")
+	os.system(f"""spotdl url {album_url} && python modules/mp3_mover.py {album_key}""")
 	print(f'Пройшло більш ніж 60 секунд з останнього завантаженного треку, альбом готовий!')
 
 
