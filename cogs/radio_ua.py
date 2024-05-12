c2b578527c7ee5768c3d9e8819cc8df39957c03e
mrartur343 <arturminesk@gmail.com>
2024-05-12 15:21:20 +0300

    bitrate bugfix

--- cogs/radio_ua.py
+++ cogs/radio_ua.py
@@ -476,7 +476,7 @@ class RadioUa(commands.Cog):  # create a class for our cog that inherits from co
 							bitrate= 128
 
 							if len(voice_channel.members)<1:
-								bitrate=32
+								bitrate=64
 
 							print(f"Bitrate: {bitrate} kb/s")
 
