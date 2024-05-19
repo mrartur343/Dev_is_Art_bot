97d8722a59f0a3183843f203ee63737409d8f321
mrartur343 <arturminesk@gmail.com>
2024-05-20 00:22:18 +0300

    some bugfixs

--- modules/album_downloader.py
+++ modules/album_downloader.py
@@ -2,7 +2,7 @@ import os
 
 
 def download_album(album_url:str,album_key:str,log_channel=None):
-	os.system(f"""spotdl download {album_url} --port 2099 --threads 10 && python modules/mp3_mover.py {album_key}""")
+	os.system(f"""spotdl download {album_url} --port 2099 --threads 20 && python modules/mp3_mover.py {album_key}""")
 	print(f'Пройшло більш ніж 60 секунд з останнього завантаженного треку, альбом готовий!')
 
 
