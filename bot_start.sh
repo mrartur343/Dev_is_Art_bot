5b048a9e70483dc48fec3560d6c2ad4a7e259f19
mrartur343 <arturminesk@gmail.com>
2024-05-25 12:55:32 +0300

    docker bugfix

--- bot_start.sh
+++ bot_start.sh
@@ -1,1 +1,6 @@
-pkill python3;git pull;nohup python3 mainBeta.py &;nohup python3 mainGamma.py &
+pkill python3
+git pull
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
+nohup python3 main.py &
+nohup python3 mainBeta.py &
+python3 mainGamma.py
