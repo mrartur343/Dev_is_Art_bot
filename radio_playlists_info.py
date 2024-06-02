23331b4de618daf4aad4d3d896e401109a80083a
mrartur343 <arturminesk@gmail.com>
2024-06-03 00:29:44 +0300

    radio_playlists_info.py

--- radio_playlists_info.py
+++ radio_playlists_info.py
@@ -10,6 +10,8 @@ with open('other/radio_playlists.json','r') as file:
 	radio_playlists: Dict[str, List[str]] = json.loads(file.read())
 with open('other/singles_names.json','r') as file:
 	singles_names: List[str] = json.loads(file.read())
+with open('other/albums_data.json','r') as file:
+	albums_data: Dict[str, List[str]] = json.loads(file.read())
 for radio_i, playlist in radio_playlists.items():
 	print(f'----- {radio_i} -----')
 	print(f'Total: {len(playlist)}')
@@ -33,3 +35,6 @@ for radio_i, playlist in radio_playlists.items():
 		all_time+=d
 	print(f'Duration: {math.floor((all_time / 60) / 60)} h {math.floor((all_time % 3600) / 60)} m {math.floor(all_time % 60)} s')
 
+	print()
+	for k in playlist:
+		print(f'{k}: {albums_data[k][0]}')
