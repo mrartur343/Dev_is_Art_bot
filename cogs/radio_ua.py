2438060a7b7b141b04621e88dcbb7e76754120f7
mrartur343 <arturminesk@gmail.com>
2024-06-02 22:24:16 +0300

    global update 1

--- cogs/radio_ua.py
+++ cogs/radio_ua.py
@@ -197,15 +197,15 @@ class GeneralRadioInfo(discord.ui.View):
 		embed.description=''
 
 		def album_line(t):
-			return f"\n{t[1][0]} ({'s' if t[0] in singles_names else 'a'}) (<t:{round(os.path.getctime(f'songs/{t[0]}'))}:d>)"
+			return f"\n{t[1][0]} ({'s' if t[0] in singles_names else 'a'})"
 
 		for t in format_tuple[:50]:
-			embed.description += album_line(t)
+			embed.add_field(name =album_line(t),value='')
 		embed2 = discord.Embed(title='Всі треки')
 		embed2.description=''
 
 		for t in format_tuple[50:]:
-			embed2.description += album_line(t)
+			embed2.add_field(name =album_line(t),value='')
 
 		embed2.set_footer(text=f'A: {len(format_tuple)-len(singles_names)}, S: {len(singles_names)}, Всього {math.floor((all_time / 60) / 60)} h {math.floor((all_time % 3600) / 60)} m {math.floor(all_time % 60)} s')
 		await interaction.respond(embeds=[embed,embed2], ephemeral=True)
