295371c1df92c2bb41f3f9174013a3193f76d80b
mrartur343 <arturminesk@gmail.com>
2024-06-03 14:43:19 +0300

    Move albums to radio

--- cogs/radio_ua.py
+++ cogs/radio_ua.py
@@ -306,7 +306,7 @@ class GeneralRadioInfo(discord.ui.View):
 			custom_view=RadioPlaylistsView(interaction_message.channel,interaction_message.id)
 		)
 
-		await radio_paginator.edit(interaction_message.interaction)
+		await radio_paginator.edit(interaction_message.message)
 
 
 class SleepTimer(discord.ui.View):
