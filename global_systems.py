5bbceee1296862ef7540ee8673495c44464f944d
mrartur343 <arturminesk@gmail.com>
2024-06-03 15:58:01 +0300

    global systems

--- global_systems.py
+++ global_systems.py
@@ -6,15 +6,11 @@ ptimer = time.time()
 
 exit_check = False
 while not exit_check:
+	time.sleep(10)
 	batery: psutil._common.sbattery = psutil.sensors_battery()
-	if time.time()-ptimer>10:
-		print(batery.percent)
-		ptimer = time.time()
-	if batery.percent<80:
-		os.system('pkill python3')
-		with open('logs/auto_pkill.txt', 'w') as file:
-			file.write(f'Auto auto pkill python3 at {datetime.datetime.now().strftime("%c")}')
-	if batery.percent<15:
+	print(f"Battery: {batery.percent}")
+	ptimer = time.time()
+	if batery.percent<25:
 		with open('logs/auto_poweroff.txt', 'w') as file:
 			file.write(f'Auto poweroff at {datetime.datetime.now().strftime("%c")}')
 		os.system('systemctl poweroff')
