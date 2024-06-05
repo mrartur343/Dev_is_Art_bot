2ebf13d4b38e5cb0e2b3fac8541e731f75698014
mrartur343 <arturminesk@gmail.com>
2024-06-05 19:42:13 +0300

    Flask Radio

--- mainFlaskRadio.py
+++ mainFlaskRadio.py
@@ -40,7 +40,7 @@ def ffmpeg_generator(fn,start_time):
 	process = Popen(["ffmpeg", "-hide_banner", "-loglevel", "panic", "-ss", str(start_time), "-i", fn, "-f", FORMAT[0], "-"], stdout=PIPE)
 	while True:
 		print("ffmpeg gen")
-		data = process.stdout.read(1024)
+		data = process.stdout.read()
 		yield data
 
 @app.route("/radio", methods=["GET"])
