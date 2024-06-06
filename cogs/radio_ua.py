8b29e6fe0e9f857242660a7dbfe3591a124cce00
mrartur343 <arturminesk@gmail.com>
2024-06-06 15:16:58 +0300

    Bot public test

--- cogs/radio_ua.py
+++ cogs/radio_ua.py
@@ -67,6 +67,7 @@ async def radio_all_play(play_source_path: str, bot: discord.Bot, radio_info_emb
 			await another_radio_info_messages[guild.id].edit(embeds=radio_info_embeds)
 		else:
 			if len(another_radio_ids[0])==1:
+				print(f"Info channel id: {another_radio_ids[0][0]}")
 				info_channel = await guild.fetch_channel(another_radio_ids[0][0])
 			else:
 				forum_channel =  await guild.fetch_channel(another_radio_ids[0][0])
