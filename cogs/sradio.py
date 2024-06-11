3fc8c3ec893e374d7809ef141abe497d8162a6eb
mrartur343 <arturminesk@gmail.com>
2024-06-12 01:02:23 +0300

    Rewrite all radio

--- cogs/sradio.py
+++ cogs/sradio.py
@@ -32,8 +32,6 @@ class RadioPlaylistsView(discord.ui.View):
 
 		radio_name = interaction.message.embeds[0].footer.text
 
-		await interaction.delete_original_message
-
 		msg = await interaction.respond(embed=discord.Embed(title='load...'))
 
 
@@ -246,7 +244,7 @@ class SRadio(commands.Cog):  # create a class for our cog that inherits from com
 			timeout=899
 		)
 
-		pmsg = await paginator.respond(ctx.interaction)
+		pmsg = await paginator.respond(ctx.interaction,ephemeral=True)
 		custom_v =RadioPlaylistsView(pmsg.channel,pmsg.id,self.bot,cycled,voice_channel)
 
 		await paginator.update(pages=embeds,custom_view=custom_v)
