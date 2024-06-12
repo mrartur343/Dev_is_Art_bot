d69bc8a85c840f74742ceb487379a9229500c693
mrartur343 <arturminesk@gmail.com>
2024-06-12 20:48:59 +0300

    Rewrite all radio

--- modules/radio_ua_views.py
+++ modules/radio_ua_views.py
@@ -115,8 +115,8 @@ class AlbumSongs(discord.ui.View):
 					items_embed.add_field(name='Сповіщення про увімкнення:', value='🔔 Увімкнуто')
 
 
-			if album_name in url_by_name:
-				song_image = sradio_contoller.track_image(url_by_name[album_name])
+			if audio_info.title in url_by_name:
+				song_image = sradio_contoller.track_image(url_by_name[audio_info.title])
 				if not (song_image is None):
 					items_embed.set_image(url=song_image)
 			items_embed.set_footer(text=album_name)
