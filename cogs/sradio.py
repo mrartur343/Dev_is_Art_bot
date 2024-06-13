692889bd98bfc09e08de357ccaaabfa84fc89ccc
mrartur343 <arturminesk@gmail.com>
2024-06-13 19:44:17 +0300

    Rewrite all radio

--- cogs/sradio.py
+++ cogs/sradio.py
@@ -392,9 +392,10 @@ class SRadio(commands.Cog):  # create a class for our cog that inherits from com
 	async def on_ready(self):  # this is called when a member joins the server
 		msg  =await (await (await self.bot.fetch_guild(1208129686031310848)).fetch_channel(1208129686572638214)).fetch_message(1250852725676310558)
 		emb = msg.embeds[0]
-		print(emb)
+		print(emb.__dict__)
 		print(f"title: {emb.title}")
 		print(f"author: {emb.author.name}")
+		print(f"desc: {emb.description}")
 		print(f"author_link: {emb.author.url}")
 		print(f"author_link_img: {emb.author.icon_url}")
 		print(f"url: {emb.author.icon_url}")
