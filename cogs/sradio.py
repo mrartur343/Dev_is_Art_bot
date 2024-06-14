e12cb2469623b40e35c750f3e31882f04cfd55a3
mrartur343 <arturminesk@gmail.com>
2024-06-14 13:57:02 +0300

    Optimization

--- cogs/sradio.py
+++ cogs/sradio.py
@@ -430,16 +430,16 @@ class SRadio(commands.Cog):  # create a class for our cog that inherits from com
 			"<#1208129687231008808> (<@1221403700115800164>)"
 		]
 
-		old_songs = []
+		old_songs, old_songs_urls = [], []
 
 		while True:
 			i = -1
 			for playlist_link in playlists_to_audit:
 				i+=1
 				if first_time[i]:
-					old_songs = sradio_contoller.get_songs(playlist_link)
+					old_songs, old_songs_urls = await sradio_contoller.get_songs(playlist_link)
 					first_time[i]=False
-				new_songs, new_songs_urls = sradio_contoller.get_songs(playlist_link)
+				new_songs, new_songs_urls = await sradio_contoller.get_songs(playlist_link)
 
 				for n_song, n_song_url in zip(new_songs, new_songs_urls):
 
@@ -447,6 +447,14 @@ class SRadio(commands.Cog):  # create a class for our cog that inherits from com
 
 					if not (n_song in old_songs):
 						await webhook.send(embed=discord.Embed(title=f"{song_info['artists'][0]['name']} - {n_song}",fields=[discord.EmbedField(name="Додано до:",value = added_to[i])],thumbnail=(await sradio_contoller.track_image(n_song_url))))
+
+
+				for o_song, o_song_url in zip(old_songs, old_songs_urls):
+
+					song_info = await sradio_contoller.get_song_info(o_song_url)
+
+					if not (o_song in new_songs):
+						await webhook.send(embed=discord.Embed(title=f"{song_info['artists'][0]['name']} - {o_song}",fields=[discord.EmbedField(name="Видалено з:",value = added_to[i])],thumbnail=(await sradio_contoller.track_image(o_song_url)),colour=discord.Colour.red()))
 
 	@commands.Cog.listener()  # we can add event listeners to our cog
 	async def on_guild_join(self, guild: discord.Guild):  # this is called when a member joins the server
