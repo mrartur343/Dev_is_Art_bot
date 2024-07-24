8bd43fe905a621432b7fdaaa7a8a57077ef7038c
mrartur343 <arturminesk@gmail.com>
2024-07-25 01:15:23 +0300

    Clean bot

--- bot_start.sh
+++
@@ -1,6 +0,0 @@
-pkill python3
-git pull
-screen python3 mainAlphaRadio.py
-screen python3 mainRadio.py 1
-screen python3 mainRadio.py 2
-screen python3 mainRadio.py 3
--- ctdl.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-track_url = sys.argv[1]
-
-print(track_url+"...")
-os.system(f'cd downloaded_songs;spotdl download {track_url} --port 8734')
-print(track_url)
