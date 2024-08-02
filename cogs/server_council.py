c42ed6a9d317e008e93f501e813bccf533e0adcb
mrartur343 <arturminesk@gmail.com>
2024-08-03 02:24:03 +0300

    Role Changes

--- cogs/server_council.py
+++ cogs/server_council.py
@@ -153,7 +153,7 @@ class ServerCouncil(commands.Cog):
 			all_requests_names = [f.split(".")[0] for f in listdir('server_requests')]
 			unused_names = all_requests_names
 			print(all_requests_names)
-			await asyncio.sleep(10)
+			await asyncio.sleep(4)
 			council_messages = await council_channel.history(limit=1000).flatten()
 			for message in council_messages:
 				try:
