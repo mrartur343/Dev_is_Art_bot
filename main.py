16d3700a00b045bdde3e7bd35fcc1e2578e866b2
mrartur343 <arturminesk@gmail.com>
2024-09-24 20:40:02 +0300

    Revert "server council"
    This reverts commit a08415ab8abd091af4b617b0de561406751e5095.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
-from cogs import interevents, voting,server_council
+from cogs import interevents, voting
 
 
 Token = "MTIyMTQwMzcwMDExNTgwMDE2NA.G-wjCw.gsVwNQwPKVbsELllSMGQwLy36iPeBbN-AnEEUI"
-TestToken = "MTI0MTQwMTg0NTU4NzM3ODIzNw.G_1Lg6.j_rEXnLx7lSV1-eM5VnaxfwNYPMHQ9iNhRrq3Q"
+
 
 intents: discord.Intents = discord.Intents.all()
 
@@ -13,8 +13,7 @@ bot = commands.Bot(intents=intents,command_prefix='....')
 
 bot.add_cog(interevents.InterEvents(bot))
 bot.add_cog(voting.VoteSystem(bot))
-bot.add_cog(server_council.ServerCouncil(bot))
 
-print("run bot!")
+print("run Alpha radio!")
 
-bot.run(TestToken)
+bot.run(Token)
