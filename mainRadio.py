1e4cf05255dcd3075ac9855ee51d58c69a9864e1
mrartur343 <arturminesk@gmail.com>
2024-09-24 20:40:40 +0300

    Revert "Beta radio (and more)"
    This reverts commit 7dae5420592d99a3226c1ecbe08e92dbd2163649.

--- mainRadio.py
+++ mainRadio.py
@@ -1,14 +1,11 @@
 import discord
 from discord.ext import commands
 import os
-from cogs import account_info,collections,event_messages,store,without_category,sradio
+from cogs import event_messages,sradio
 import sys
 
 
-if sys.argv[1]=='a':
-	Token = "MTIyMTQwMzcwMDExNTgwMDE2NA.G-wjCw.gsVwNQwPKVbsELllSMGQwLy36iPeBbN-AnEEUI"
-else:
-	Token = 'MTI0MTQwMTg0NTU4NzM3ODIzNw.GnPixU.USNyu1X811MNAKgw06ZOBVwutH5bgOfunbjl9I'
+Token = sys.argv[1]
 intents: discord.Intents = discord.Intents.all()
 
 bot = commands.Bot(intents=intents,command_prefix='....')
@@ -16,14 +13,13 @@ print(f"cogs: {os.listdir('./cogs')}")
 
 
 bot.add_cog(sradio.SRadio(bot))
-if sys.argv[1]=='a':
+if Token=="MTIyMTQwMzcwMDExNTgwMDE2NA.G-wjCw.gsVwNQwPKVbsELllSMGQwLy36iPeBbN-AnEEUI":
 	bot.add_cog(event_messages.EventsMessages(bot))
-
 #bot.add_cog(account_info.Account(bot))
 #bot.add_cog(collections.Collections(bot))
 #bot.add_cog(store.Store(bot))
 #bot.add_cog(without_category.WithoutCategory(bot))
 
-print("run Alpha radio!")
+print("run radio!")
 
 bot.run(Token)
