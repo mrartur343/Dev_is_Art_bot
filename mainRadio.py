faca59503490249c431f25fe02450eb9afd1320b
mrartur343 <arturminesk@gmail.com>
2024-09-24 20:40:43 +0300

    Revert "Beta radio (and more)"
    This reverts commit 585de996195847185ac887b84dfe05da021058ef.

--- mainRadio.py
+++ mainRadio.py
@@ -2,10 +2,8 @@ import discord
 from discord.ext import commands
 import os
 from cogs import account_info,collections,event_messages,store,without_category,sradio
-import sys
 
-
-Token = sys.argv[1]
+Token = "MTIyMTQwMzcwMDExNTgwMDE2NA.G-wjCw.gsVwNQwPKVbsELllSMGQwLy36iPeBbN-AnEEUI"
 intents: discord.Intents = discord.Intents.all()
 
 bot = commands.Bot(intents=intents,command_prefix='....')
@@ -13,13 +11,13 @@ print(f"cogs: {os.listdir('./cogs')}")
 
 
 bot.add_cog(sradio.SRadio(bot))
-if Token=="MTIyMTQwMzcwMDExNTgwMDE2NA.G-wjCw.gsVwNQwPKVbsELllSMGQwLy36iPeBbN-AnEEUI":
-	bot.add_cog(event_messages.EventsMessages(bot))
+bot.add_cog(event_messages.EventsMessages(bot))
+
 #bot.add_cog(account_info.Account(bot))
 #bot.add_cog(collections.Collections(bot))
 #bot.add_cog(store.Store(bot))
 #bot.add_cog(without_category.WithoutCategory(bot))
 
-print("run radio!")
+print("run Alpha radio!")
 
 bot.run(Token)
