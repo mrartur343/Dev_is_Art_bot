afdfb8a14196c45c9c83d0809c5c99d951edbe85
mrartur343 <arturminesk@gmail.com>
2024-09-24 20:38:36 +0300

    Revert "Error defence"
    This reverts commit 37c853894fa1e112e2e85dadf918d5230ee543f1.

--- cogs/server_council.py
+++ cogs/server_council.py
@@ -150,195 +150,191 @@ class ServerCouncil(commands.Cog):
 		council_messages = await council_channel.history(limit=1000).flatten()
 
 		while True:
-			try:
-				all_requests_names = [f.split(".")[0] for f in listdir('server_requests')]
-				unused_names = all_requests_names
-				print(all_requests_names)
-				council_messages = await council_channel.history(limit=1000).flatten()
-				for message in council_messages:
-					try:
-						if len(message.embeds)>0:
-							embed = message.embeds[0]
-							if message.author.id==self.bot.user.id and os.path.exists(f'server_requests/{embed.title}.json'):
-								await message.edit(view=VoteView(message.embeds[0].title))
-								if embed.title in unused_names:
-									unused_names.remove(embed.title)
+			all_requests_names = [f.split(".")[0] for f in listdir('server_requests')]
+			unused_names = all_requests_names
+			print(all_requests_names)
+			council_messages = await council_channel.history(limit=1000).flatten()
+			for message in council_messages:
+				try:
+					if len(message.embeds)>0:
+						embed = message.embeds[0]
+						if message.author.id==self.bot.user.id and os.path.exists(f'server_requests/{embed.title}.json'):
+							await message.edit(view=VoteView(message.embeds[0].title))
+							if embed.title in unused_names:
+								unused_names.remove(embed.title)
 
-								if not message.pinned:
-									await message.pin()
+							if not message.pinned:
+								await message.pin()
+							with open(f'server_requests/{embed.title}.json', 'r') as file:
+								server_request_dict = json.loads(file.read())
+
+							timestamp: int = server_request_dict['timestamp']
+							voting: Dict[str, int] = server_request_dict['voting']
+							if 'comment' in server_request_dict:
+								comment: str = server_request_dict['comment']
+							elif 'add_roles' in server_request_dict:
+								add_roles_str = server_request_dict['add_roles']
+								if len(add_roles_str)>0:
+									if add_roles_str[-1]==' ':
+										add_roles_str = add_roles_str[:-1]
+								add_roles_id: List[int] = add_roles_str.split(" ")
+								if len(add_roles_id)==1:
+									if add_roles_id[0]=='':
+										add_roles_id.pop(0)
+								print(add_roles_id)
+								remove_roles_str = server_request_dict['remove_roles']
+								if len(remove_roles_str)>0:
+									if remove_roles_str[-1]==' ':
+										remove_roles_str = remove_roles_str[:-1]
+								remove_roles_id: List[int] = remove_roles_str.split(" ")
+								if len(remove_roles_id)==1:
+									if remove_roles_id[0]=='':
+										remove_roles_id.pop(0)
+								print(remove_roles_id)
+								comment = ''
+
+								for role_id in add_roles_id:
+									comment+=f'\n Додати <@&{role_id}>'
+
+								comment+='\n'
+
+								for role_id in remove_roles_id:
+									comment+=f'\n Забрати <@&{role_id}>'
+
+							print(f"Time check for {embed.title} - {(datetime.datetime.now().timestamp() - timestamp)-60*60*24} seconds")
+							if ((datetime.datetime.now().timestamp() - timestamp)>=60*60*24) or (not (0 in voting.values())) or (list(voting.values()).count(1) > list(voting.values()).count(0)+list(voting.values()).count(2)) or (list(voting.values()).count(2) > list(voting.values()).count(0)+list(voting.values()).count(1)):
+								print(f"END {embed.title}")
+
+
+								y = 0
+								n = 0
+								h = 0
+								d = 0
+								for v in voting.values():
+									if v == 1:
+										y+=1
+									elif v == 2:
+										n+=1
+									elif v == 3:
+										h += 1
+									else:
+										d += 1
+
+
+
+								if y==n:
+									await council_channel.send(f"Голосування по запиту {embed.title} завершилось тим, що рада серверу не знайшла однозначного рішення\n\n"
+									                           f"❌ Пропозицію не прийнято\n"
+									                           f"> - {y} - Підтримали\n"
+									                           f"> - {n} - Не підтримали\n"
+									                           f"> - {h} - Утримались\n"
+									                           f"> - {d} - Не проголосували", embed=discord.Embed(title=embed.title,description=comment, colour=discord.Colour.from_rgb(79,84,92)))
+								elif y>n:
+									await council_channel.send(f"Голосування по запиту {embed.title} завершилось прийняттям\n\n"
+									                           f"✅ Пропозицію прийнято\n"
+									                           f"> - {y} - Підтримали\n"
+									                           f"> - {n} - Не підтримали\n"
+									                           f"> - {h} - Утримались\n"
+									                           f"> - {d} - Не проголосували", embed=discord.Embed(title=embed.title,description=comment, colour=discord.Colour.from_rgb(79,84,92)))
+									if 'add_roles' in server_request_dict:
+										target_member = await council_channel.guild.fetch_member(server_request_dict['target'])
+
+										add_roles: List[discord.Role] = []
+										for role_id in add_roles_id:
+											await target_member.add_roles(await council_channel.guild._fetch_role(int(role_id)))
+
+										remove_roles: List[discord.Role] = []
+										for role_id in remove_roles_id:
+											await target_member.remove_roles(await council_channel.guild._fetch_role(int(role_id)))
+
+
+
+								elif n>y:
+									await council_channel.send(f"Голосування по запиту {embed.title} завершилось не прийняттям\n\n"
+									                           f"❌ Пропозицію не прийнято\n"
+									                           f"> - {y} - Підтримали\n"
+									                           f"> - {n} - Не підтримали\n"
+									                           f"> - {h} - Утримались\n"
+									                           f"> - {d} - Не проголосували", embed=discord.Embed(title=embed.title,description=comment, colour=discord.Colour.from_rgb(79,84,92)))
+
+								await message.delete()
+								os.replace(f'server_requests/{embed.title}.json',f'ended_requests/{embed.title}.json')
+							else:
 								with open(f'server_requests/{embed.title}.json', 'r') as file:
-									server_request_dict = json.loads(file.read())
+									voting: Dict[str,int] = json.loads(file.read())['voting']
 
-								timestamp: int = server_request_dict['timestamp']
-								voting: Dict[str, int] = server_request_dict['voting']
-								if 'comment' in server_request_dict:
-									comment: str = server_request_dict['comment']
-								elif 'add_roles' in server_request_dict:
-									add_roles_str = server_request_dict['add_roles']
-									if len(add_roles_str)>0:
-										if add_roles_str[-1]==' ':
-											add_roles_str = add_roles_str[:-1]
-									add_roles_id: List[int] = add_roles_str.split(" ")
-									if len(add_roles_id)==1:
-										if add_roles_id[0]=='':
-											add_roles_id.pop(0)
-									print(add_roles_id)
-									remove_roles_str = server_request_dict['remove_roles']
-									if len(remove_roles_str)>0:
-										if remove_roles_str[-1]==' ':
-											remove_roles_str = remove_roles_str[:-1]
-									remove_roles_id: List[int] = remove_roles_str.split(" ")
-									if len(remove_roles_id)==1:
-										if remove_roles_id[0]=='':
-											remove_roles_id.pop(0)
-									print(remove_roles_id)
-									comment = ''
+								voters = []
 
-									for role_id in add_roles_id:
-										comment+=f'\n Додати <@&{role_id}>'
+								for k, v in voting.items():
+									if v!=0:
+										voters.append(int(k))
 
-									comment+='\n'
+								old_voters_str = embed.fields[1].value
+								new_voters_str = '*(ніхто)*'
+								if len(voters)>0:
+									new_voters_str=''
+								for vtr in voters:
+									new_voters_str+=f"<@{vtr}> "
 
-									for role_id in remove_roles_id:
-										comment+=f'\n Забрати <@&{role_id}>'
+								if new_voters_str!=old_voters_str:
+									embed.fields[1].value = new_voters_str
+									await message.edit(embed = embed)
+				except Exception as exception:
+					print(exception.__str__())
 
-								print(f"Time check for {embed.title} - {(datetime.datetime.now().timestamp() - timestamp)-60*60*24} seconds")
-								if ((datetime.datetime.now().timestamp() - timestamp)>=60*60*24) or (not (0 in voting.values())) or (list(voting.values()).count(1) > list(voting.values()).count(0)+list(voting.values()).count(2)) or (list(voting.values()).count(2) > list(voting.values()).count(0)+list(voting.values()).count(1)):
-									print(f"END {embed.title}")
+			print(f'Unused: {unused_names}')
+			for request_name in unused_names:
+				try:
+					embed = discord.Embed(title=request_name)
+					with open(f'server_requests/{request_name}.json', 'r') as file:
+						request_info: Dict[str, Any] = json.loads(file.read())
 
+					start_timestamp= request_info['timestamp']
+					author_id= request_info['author_id']
+					if 'voting' in request_info:
+						del(request_info['voting'])
+						del(request_info['timestamp'])
+						del(request_info['author_id'])
+					embed.description=''
+					nl = '\n'
+					if 'comment' in request_info:
+						for k,v in request_info.items():
+							embed.description+=f"\n- {k}\n> {v.replace(nl, nl+'> ')}"
+					else:
+						add_roles_id: List[int] = request_info['add_roles'].split(" ")
+						remove_roles_id: List[int] = request_info['remove_roles'].split(" ")
+						target: int = request_info['target']
+						comment = f'Змінити ролі для <@{target}>'
 
-									y = 0
-									n = 0
-									h = 0
-									d = 0
-									for v in voting.values():
-										if v == 1:
-											y+=1
-										elif v == 2:
-											n+=1
-										elif v == 3:
-											h += 1
-										else:
-											d += 1
+						comment += '\n'
 
+						for role_id in add_roles_id:
+							if role_id!='':
+								comment += f'\n Додати <@&{role_id}>'
 
+						comment += '\n'
 
-									if y==n:
-										await council_channel.send(f"Голосування по запиту {embed.title} завершилось тим, що рада серверу не знайшла однозначного рішення\n\n"
-										                           f"❌ Пропозицію не прийнято\n"
-										                           f"> - {y} - Підтримали\n"
-										                           f"> - {n} - Не підтримали\n"
-										                           f"> - {h} - Утримались\n"
-										                           f"> - {d} - Не проголосували", embed=discord.Embed(title=embed.title,description=comment, colour=discord.Colour.from_rgb(79,84,92)))
-									elif y>n:
-										await council_channel.send(f"Голосування по запиту {embed.title} завершилось прийняттям\n\n"
-										                           f"✅ Пропозицію прийнято\n"
-										                           f"> - {y} - Підтримали\n"
-										                           f"> - {n} - Не підтримали\n"
-										                           f"> - {h} - Утримались\n"
-										                           f"> - {d} - Не проголосували", embed=discord.Embed(title=embed.title,description=comment, colour=discord.Colour.from_rgb(79,84,92)))
-										if 'add_roles' in server_request_dict:
-											target_member = await council_channel.guild.fetch_member(server_request_dict['target'])
+						for role_id in remove_roles_id:
+							if role_id!='':
+								comment += f'\n Забрати <@&{role_id}>'
 
-											add_roles: List[discord.Role] = []
-											for role_id in add_roles_id:
-												await target_member.add_roles(await council_channel.guild._fetch_role(int(role_id)))
+						embed.description=comment
 
-											remove_roles: List[discord.Role] = []
-											for role_id in remove_roles_id:
-												await target_member.remove_roles(await council_channel.guild._fetch_role(int(role_id)))
+					embed.add_field(name="Кінець голосування: ",
+					                value=f'<t:{round((datetime.datetime.fromtimestamp(start_timestamp)+datetime.timedelta(seconds=60*60*24)).timestamp())}:R> (<t:{round((datetime.datetime.fromtimestamp(start_timestamp)+datetime.timedelta(seconds=60*60*24)).timestamp())}:d> <t:{round((datetime.datetime.fromtimestamp(start_timestamp)+datetime.timedelta(seconds=60*60*24)).timestamp())}:t>)')
+					embed.add_field(name="Проголосували: ",
+					                value=f'*(ніхто)*')
+					embed.add_field(name="Створив запит: ",
+					                value=f'<@{author_id}>')
 
-
-
-									elif n>y:
-										await council_channel.send(f"Голосування по запиту {embed.title} завершилось не прийняттям\n\n"
-										                           f"❌ Пропозицію не прийнято\n"
-										                           f"> - {y} - Підтримали\n"
-										                           f"> - {n} - Не підтримали\n"
-										                           f"> - {h} - Утримались\n"
-										                           f"> - {d} - Не проголосували", embed=discord.Embed(title=embed.title,description=comment, colour=discord.Colour.from_rgb(79,84,92)))
-
-									await message.delete()
-									os.replace(f'server_requests/{embed.title}.json',f'ended_requests/{embed.title}.json')
-								else:
-									with open(f'server_requests/{embed.title}.json', 'r') as file:
-										voting: Dict[str,int] = json.loads(file.read())['voting']
-
-									voters = []
-
-									for k, v in voting.items():
-										if v!=0:
-											voters.append(int(k))
-
-									old_voters_str = embed.fields[1].value
-									new_voters_str = '*(ніхто)*'
-									if len(voters)>0:
-										new_voters_str=''
-									for vtr in voters:
-										new_voters_str+=f"<@{vtr}> "
-
-									if new_voters_str!=old_voters_str:
-										embed.fields[1].value = new_voters_str
-										await message.edit(embed = embed)
-					except Exception as exception:
-						print(exception.__str__())
-
-				print(f'Unused: {unused_names}')
-				for request_name in unused_names:
-					try:
-						embed = discord.Embed(title=request_name)
-						with open(f'server_requests/{request_name}.json', 'r') as file:
-							request_info: Dict[str, Any] = json.loads(file.read())
-
-						start_timestamp= request_info['timestamp']
-						author_id= request_info['author_id']
-						if 'voting' in request_info:
-							del(request_info['voting'])
-							del(request_info['timestamp'])
-							del(request_info['author_id'])
-						embed.description=''
-						nl = '\n'
-						if 'comment' in request_info:
-							for k,v in request_info.items():
-								embed.description+=f"\n- {k}\n> {v.replace(nl, nl+'> ')}"
-						else:
-							add_roles_id: List[int] = request_info['add_roles'].split(" ")
-							remove_roles_id: List[int] = request_info['remove_roles'].split(" ")
-							target: int = request_info['target']
-							comment = f'Змінити ролі для <@{target}>'
-
-							comment += '\n'
-
-							for role_id in add_roles_id:
-								if role_id!='':
-									comment += f'\n Додати <@&{role_id}>'
-
-							comment += '\n'
-
-							for role_id in remove_roles_id:
-								if role_id!='':
-									comment += f'\n Забрати <@&{role_id}>'
-
-							embed.description=comment
-
-						embed.add_field(name="Кінець голосування: ",
-						                value=f'<t:{round((datetime.datetime.fromtimestamp(start_timestamp)+datetime.timedelta(seconds=60*60*24)).timestamp())}:R> (<t:{round((datetime.datetime.fromtimestamp(start_timestamp)+datetime.timedelta(seconds=60*60*24)).timestamp())}:d> <t:{round((datetime.datetime.fromtimestamp(start_timestamp)+datetime.timedelta(seconds=60*60*24)).timestamp())}:t>)')
-						embed.add_field(name="Проголосували: ",
-						                value=f'*(ніхто)*')
-						embed.add_field(name="Створив запит: ",
-						                value=f'<@{author_id}>')
-
-						uids_str = ''
-						for member in (await council_channel.guild._fetch_role(council_role_id)).members:
-							uids_str+=f"<@{member.id}> "
-						await council_channel.send(f'Можуть проголосувати: \n{uids_str}',embed=embed,view=VoteView(request_name, timeout=None))
-					except Exception as e:
-						print("ERROR")
-						print(e.__str__())
-				await asyncio.sleep(60)
-			except Exception as e:
-				with open(f'ERROR_LOG_{round(datetime.datetime.now().timestamp())}', 'w') as file:
-					json.dump(['Error in cycle start', e.__str__(), datetime.datetime.now().strftime("%c")],file)
+					uids_str = ''
+					for member in (await council_channel.guild._fetch_role(council_role_id)).members:
+						uids_str+=f"<@{member.id}> "
+					await council_channel.send(f'Можуть проголосувати: \n{uids_str}',embed=embed,view=VoteView(request_name, timeout=None))
+				except Exception as e:
+					print("ERROR")
+					print(e.__str__())
+			await asyncio.sleep(60)
 	@discord.slash_command()  # we can also add application commands
 	@commands.has_permissions(administrator=True)
 	async def clean(self, ctx: discord.ApplicationContext, num: int):
