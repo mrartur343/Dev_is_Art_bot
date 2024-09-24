710fefe8e6ce63a62f00a8d6cf7fa6b3cf088015
mrartur343 <arturminesk@gmail.com>
2024-09-24 20:40:19 +0300

    Revert "Beta radio (and more)"
    This reverts commit 0d86ecbcd897ee47d635dfb7b615f99fe6fe9816.

--- sradio_contoller.py
+++ sradio_contoller.py
@@ -46,8 +46,7 @@ def get_all_songs_paths() -> typing.Tuple[typing.List[str],typing.List[str]]:
 	with open('other/songs_names_cache.json','r')as file:
 		cache = json.loads(file.read())
 		for f in all_songs_files:
-			if f in cache:
-				all_songs_names.append(cache[f])
+			all_songs_names.append(cache[f])
 
 
 
